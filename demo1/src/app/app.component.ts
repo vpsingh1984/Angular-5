643d259074286371631d56a48b6694a985e770ae
Vijay Singh2 <vijay.singh2@synapse.com>
2018-02-24 12:24:24 +0530

    new component added and used Input

--- demo1/src/app/app.component.ts
+++ demo1/src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: `
+    <input #myInput type="text">
+    <button type="button" (click)="logMessage(myInput.value)">Log</button>
+    <app-test [parentData]="name"></app-test>
+    `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+  public name = "Vijay";
+  
+  logMessage(val) {
+    console.log(val);
+  }
 }
